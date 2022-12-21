f0cd36f80f896887f0b95b04e507b8edcae9296f
Jeremiah Desktop <jereberries1215@gmail.com>
2022-12-20 21:49:34 -0800

    got add entry button functionable

--- scripts.js
+++ scripts.js
@@ -1,6 +1,51 @@
 //query selectors
 let addEntryBtn = document.querySelector('.add-prompt-button');
+let submitBtn = document.querySelector('.submit-button');
+let textBox = document.querySelector('#entry');
+let prompt = document.querySelector('#prompt');
 //event listeners
 addEntryBtn.addEventListener("click", () => {
     window.scrollTo(0, 0);
+});
+submitBtn.addEventListener("click", () => {
+    console.log("submit button clicked");
+    console.log(textBox.value);
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className = "delete";
+    let editBtn = document.createElement("button");
+    editBtn.className = "edit";
+    editBtn.textContent = "edit";
+    let actions = document.createElement("div");
+    actions.className = "actions";
+    actions.appendChild(editBtn);
+    actions.appendChild(deleteBtn);
+
+    let answer = document.createElement("div");
+    answer.className = "answer";
+    answer.textContent = textBox.value;
+    let promptTitle = document.createElement("h3");
+    promptTitle.className = "prompt-title";
+    promptTitle.textContent = prompt.textContent;
+    let entryBox = document.createElement("div");
+    entryBox.className = "entry-box";
+    entryBox.appendChild(promptTitle);
+    entryBox.appendChild(answer);
+
+    let date = document.createElement("h4");
+    date.className = "date";
+    date.textContent = "Tue, May 24";
+    
+    let entryCard = document.createElement("div");
+    entryCard.className = "entry-card";
+    entryCard.appendChild(date);
+    entryCard.appendChild(entryBox);
+   
+
+    let itemEntry = document.createElement("div");
+    itemEntry.className = "item-entry";
+    itemEntry.appendChild(entryCard);
+    itemEntry.appendChild(actions);
+    document.querySelector('.entry-list').appendChild(itemEntry);
+    textBox.value = '';
 });
